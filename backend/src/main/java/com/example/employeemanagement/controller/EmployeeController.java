2344122b8bc29af2f8ac35b8707ab41cdf52bf20
Siva360p <siva360@gmail.com>
2026-06-03 00:34:51 +0530

    changed controller

--- backend/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ backend/src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(
+    origins =
+    "https://employee-management-system-4n5p.vercel.app"
+)
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
